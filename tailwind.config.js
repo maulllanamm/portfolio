1f27d2a1317db745dcf983628ff498d87d2f9da5
maulllanamm <maulllanamuhammad@gmail.com>
2022-05-22 19:22:34 +0700

    Hero section

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,9 @@ module.exports = {
   content: ["index.html"],
   theme: {
     extend: {
+      container: {
+        center: true,
+      },
       fontFamily: {
         inter: ["Inter"],
       },
